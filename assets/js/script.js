57674122948893b17afa4fa34ab5e5027a8a7cf1
SeanLP <seanpreston9213@gmail.com>
2021-10-18 16:38:15 +0000

    Add code to decide game winner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,96 @@
-document.addEventListener("DOMContentLoaded", function() {
+// document.addEventListener("DOMContentLoaded", function() {
 
-    const buttons = document.getElementsByTagName("button");
+//     const buttons = document.getElementsByTagName("button");
+//     const result = document.getElementById("result");
 
-    for (let button of buttons) {
-        button.addEventListener("click", getUserChoice);
+//     for (let button of buttons) {
+//         button.addEventListener("click", getUserChoice);
 
-    }
+//     }
 
 
-});
+// });
 
-function getUserChoice() {
+// function getUserChoice() {
 
-    const userChoice = document.getElementById("user-choice");
+//     const userChoice = document.getElementById("user-choice");
 
-    userChoice.innerText = this.getAttribute("id");
+//     userChoice.innerText = this.getAttribute("id");
 
-    computerChoice();
+//     computerChoice();
 
-}
+// }
 
-function computerChoice() {
+// function computerChoice() {
     
-    const choices = ["Rock", "Paper", "Scissors"];
+//     const choices = ["Rock", "Paper", "Scissors"];
 
-    const random = Math.floor(Math.random() * 3);
+//     const computerInput = choices[Math.floor(Math.random() * 3)];
 
-    document.getElementById("computer-choice").innerHTML = `${choices[random]}`;
-}
+//     document.getElementById("computer-choice").innerHTML = `${computerInput}`;
+// }
+
+const choices = document.querySelectorAll('.choice');
+const score = document.getElementById('scores');
+const result = document.getElementById('result');
+const playerInput = document.getElementById('user-choice');
+const compInput = document.getElementById('computer-choice');
+
+// const scoreboard = {
+//     player: 0,
+//     computer: 0
+//   };
+
+  function playGame(e) {
+      
+    const playerChoice = e.target.id;
+    playerInput.innerHTML = playerChoice;
+    const computerChoice = getComputerChoice();
+    compInput.innerHTML = computerChoice;
+    const winner = getWinner(playerChoice, computerChoice);
+
+    console.log(playerChoice, computerChoice, winner)
+    result.innerHTML = winner;
+
+  }
+
+  function getComputerChoice() {
+
+    const random = Math.random();
+
+    if (random < 0.34) {
+        return 'Rock';
+    } else if (random <= 0.67) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+  }
+
+  function getWinner(playerChoice, computerChoice) {
+
+    if (playerChoice === computerChoice) {
+        return 'Draw';
+    } else if (playerChoice === 'Rock') {
+        if (computerChoice === 'Paper') {
+            return 'You Lose';
+
+        } else {
+            return 'You Win';
+        }
+    } else if (playerChoice === 'Paper') {
+        if (computerChoice === 'Scissors') {
+            return 'You Lose';
+        } else {
+            return 'You Win';
+        }
+    } else if (playerChoice === 'Scissors') {
+        if (computerChoice === 'Rock') {
+            return 'You Lose';
+        } else {
+            return 'You Win';
+        }
+    }
+  }
+
+  choices.forEach(choice => choice.addEventListener('click', playGame));
