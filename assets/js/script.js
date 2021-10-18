09b0383461f3b08176f327a44e6074f40021449a
SeanLP <seanpreston9213@gmail.com>
2021-10-18 12:45:11 +0000

    Add code to generate computer random choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
     for (let button of buttons) {
         button.addEventListener("click", getUserChoice);
+
     }
+
 
 });
 
@@ -14,4 +16,15 @@ function getUserChoice() {
 
     userChoice.innerText = this.getAttribute("id");
 
+    computerChoice();
+
+}
+
+function computerChoice() {
+    
+    const choices = ["Rock", "Paper", "Scissors"];
+
+    const random = Math.floor(Math.random() * 3);
+
+    document.getElementById("computer-choice").innerHTML = `${choices[random]}`;
 }
